68fedc581ec90630e944f9e0314a452d8d9e2640
Geisianny <geisiannybernardo@gmail.com>
2023-09-04 00:59:00 -0300

    implementando a base do chat

---
+++ projeto-chat-docker/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+const path = require('path'); // Importe o módulo "path"
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+// Configurando a rota para a página inicial
+app.get('/', (req, res) => {
+     res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+wss.on('connection', (ws) => {
+    ws.on('message', (message) => {
+        wss.clients.forEach((client) => {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(message);
+            }
+        });
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Server is listening on port 3000');
+});
