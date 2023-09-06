428592411d9ec52d7905b4b62ad45408950dc0aa
Geisianny <geisiannybernardo@gmail.com>
2023-09-06 00:28:31 -0300

    adicionando documentacao do projeto

--- projeto-chat-docker/server.js
+++ projeto-chat-docker/server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const http = require('http');
 const WebSocket = require('ws');
+const path = require('path'); // Importe o módulo "path"
 
 const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
+
+// Configurando a rota para a página inicial
+app.get('/', (req, res) => {
+     res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 wss.on('connection', (ws) => {
     ws.on('message', (message) => {
